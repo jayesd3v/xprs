f4a044866eacd839983f16b9410885b274307821
jayesd3v <jayesd3v@gmail.com>
2025-02-12 00:02:28 +0700

    chore: added custom-loader

---
+++ scripts/custom-loader/customLoader.js
@@ -0,0 +1,8 @@
+import path from 'path';
+
+const ROOT_PATH = new URL(path.dirname(import.meta.url) + '/src').pathname;
+
+export function resolve(specifier, parentModuleURL, defaultResolver) {
+    specifier = specifier.replace(/^@/, ROOT_PATH);
+    return defaultResolver(specifier, parentModuleURL);
+}
