84a93082131a2bebde148bdb13e1586e2823ca11
jayesd3v <jayesd3v@gmail.com>
2025-02-12 00:04:03 +0700

    docs: added comments for customLoader

--- scripts/custom-loader/customLoader.js
+++ scripts/custom-loader/customLoader.js
@@ -2,6 +2,7 @@ import path from 'path';
 
 const ROOT_PATH = new URL(path.dirname(import.meta.url) + '/src').pathname;
 
+// this help project to resolve the path from src folder instead of root, then we can import with the relative path from src folder
 export function resolve(specifier, parentModuleURL, defaultResolver) {
     specifier = specifier.replace(/^@/, ROOT_PATH);
     return defaultResolver(specifier, parentModuleURL);
