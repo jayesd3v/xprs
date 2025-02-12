e166a955557e3a4940479554e9e94587a43cd7f8
jayesd3v <jayesd3v@gmail.com>
2025-02-12 12:07:10 +0700

    chore: update custom loader to support relative path, folder import and implicitly js file import

--- scripts/custom-loader/customLoader.js
+++ scripts/custom-loader/customLoader.js
@@ -1,9 +1,66 @@
 import path from 'path';
+import fs from 'fs';
+import clc from 'cli-color';
 
-const ROOT_PATH = new URL(path.dirname(import.meta.url) + '../../../src').pathname;
+const ROOT_PATH =
+    path.dirname(import.meta.url).split('/scripts/custom-loader')[0] + '/src';
 
-// this help project to resolve the path from src folder instead of root, then we can import with the relative path from src folder
-export function resolve(specifier, parentModuleURL, defaultResolver) {
-    specifier = specifier.replace(/^@/, ROOT_PATH);
-    return defaultResolver(specifier, parentModuleURL);
+export function resolveSpecifier(specifier, isDebug) {
+    if (isDebug) {
+        console.log('Original specifier:', clc.blue(specifier));
+    }
+    if (specifier.startsWith('@/')) {
+        specifier = specifier.replace(/^@/, ROOT_PATH);
+
+        if (isDebug) {
+            console.log(
+                'The specifier start with @/, so we replace it with ROOT_PATH:',
+                clc.cyanBright(specifier),
+            );
+        }
+
+        // on Windows, the path scheme won't work with lstatSync, se we remove it first
+        const checkingPath = specifier.replace(/^file:(\/)+/, '');
+
+        const isExist = fs.existsSync(checkingPath);
+
+        if (isDebug) {
+            console.log('Checking path:', clc.cyanBright(checkingPath));
+        }
+
+        if (isExist && fs.lstatSync(checkingPath).isDirectory()) {
+            if (isDebug) {
+                console.log('Path is directory');
+            }
+
+            specifier = `${specifier}/index.js`;
+        } else if (!isExist) {
+            const checkingPathWithExtension = `${checkingPath}.js`;
+            const isExistWithExtension = fs.existsSync(
+                checkingPathWithExtension,
+            );
+
+            if (isDebug) {
+                console.log(
+                    `Path is not exist, try to check with .js extension: ${clc.cyanBright(checkingPathWithExtension)}`,
+                );
+            }
+
+            if (
+                isExistWithExtension &&
+                fs.lstatSync(checkingPathWithExtension).isFile()
+            ) {
+                if (isDebug) {
+                    console.log('Path is file');
+                }
+                specifier = `${specifier}.js`;
+            }
+        }
+    }
+
+    return specifier;
+}
+
+export function resolve(specifier, parentModuleURL, defaultResolver, isDebug) {
+    return defaultResolver(resolveSpecifier(specifier), parentModuleURL);
 }
