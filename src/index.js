4a595ab98150a8ad3ad7cf7d756c42d58a3b5b10
jayesd3v <jayesd3v@gmail.com>
2025-03-29 03:08:45 +0700

    feat: call sequelize initializer on index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import App from '@/server/app';
+import { initializeSequelize } from '@/models';
+import { env } from '@/utils';
+
+const { NODE_ENV } = env;
+
+if (NODE_ENV === 'dev') {
+    initializeSequelize({
+        alter: true,
+        force: false,
+        logging: false,
+    });
+}
 
 const app = new App();
 app.start();
