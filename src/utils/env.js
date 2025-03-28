33e131a2d92ea858585282a4b55f65636c450c90
jayesd3v <jayesd3v@gmail.com>
2025-03-29 03:07:45 +0700

    refactor: stop parsing data from env to prevent data confusing in the future

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
-import parse from 'dotenv-parse-variables';
 
-const env = parse(process.env);
+const env = process.env;
 env.ROOT_PATH = process.cwd();
 
 export default env;
