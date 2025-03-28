fed4d09b482b4f2992f80261554f5ef1dca702b9
jayesd3v <jayesd3v@gmail.com>
2025-03-29 03:07:08 +0700

    feat: add default export for models folder

---
+++ src/models/index.js
@@ -0,0 +1,3 @@
+export { default as sequelize } from '@/models/sequelize';
+export { default as sampleModel } from '@/models/sample.model';
+export { default as initializeSequelize } from '@/models/initializeSequelize';
